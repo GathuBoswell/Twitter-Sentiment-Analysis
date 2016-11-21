18abfc44cf841a980e2479738157aaee55cb7a68
GathuBoswell <gathudev@gmail.com>
2016-11-21 19:39:22 +0300

    Initial Commmit

---
+++ twittersent_analysis_test.py
@@ -0,0 +1,13 @@
+import unittest
+
+import twitter_data
+
+
+class TwitterDataTestCase(unittest.TestCase):
+    def test_twiter_data_instance(self):
+        tweets = twitter_data.GetData()
+        self.assertEqual(type(tweets), twitter_data.GetData())
+
+
+if __name__ == '__main__':
+    unittest.main()
