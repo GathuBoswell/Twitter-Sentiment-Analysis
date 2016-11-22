c4317d13706c117fef76a1a476066300c85dabfa
GathuBoswell <gathudev@gmail.com>
2016-11-22 12:11:41 +0300

    Add word_list() function to return a list of words sieved from the twiter tweets text

--- twitter_data.py
+++ twitter_data.py
@@ -33,18 +33,37 @@ class GetData(object):
             json.dump(status_list, json_data)
         return ''
 
-    def word_frequency(self):
+    def word_list(self):
         import json
-        from pprint import pprint
+        import re
 
         tweet_list = []
         with open(self._json_file, 'r') as tweet_data:
             data = json.load(tweet_data)
-        tweet_list.append(data)
-        # print(len(data))
-        # pprint(data)
-        for item in data:
-            print (item['text'])
+        tweet_list.extend(data)
+        # print(len(tweet_list))
+        # pprint(tweet_list)
+        unwanted_texts = re.compile('[@#."]')
+        words = []
+        for status in tweet_list:
+            for word in status['text'].split():
+                # create a regex to catch this class ['@#."']
+                #if word.startswith('#') or word.startswith('@') or word.startswith('http'):
+                if unwanted_texts.match(word) or word.startswith('http'):
+                    continue
+                else:
+                    for word_members in (word.replace(':',' ').replace('.',' '
+                                        ).replace('!',' ').replace('?',' '
+                                        ).replace('"', ' '
+                                        ).replace('/',' ').split()
+                                        ) or word.startswith('http'):
+                    # word_members = re.findall(r"[\w]+", word)
+                    # words.extend(word_members)
+                        words.append(word_members)
+        print(words)
+
+        def word_count(self):
+            pass
 
 def main():
     cons_key = "qrRMCNNtOtWlN7YPAwllY4C9p"
@@ -52,7 +71,7 @@ def main():
     access_key = "765074739228471296-eQswENirBvmzVSI3LNSf7p7E3r4L32d"
     access_secret = "t2SuOHDGxO8T5EzaEcoM17mu6ug65F9TGdeo8L8NnT46a"
     data = GetData(cons_key, cons_secret, access_key, access_secret)
-    tweets = data.get_tweets('@allelachavo')
+    #tweets = data.get_tweets('@allelachavo')
     print(data.word_frequency())
 
 if __name__ == '__main__':main()
