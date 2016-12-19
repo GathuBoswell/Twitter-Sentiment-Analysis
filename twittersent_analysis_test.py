a1784b27c4e4347a732839bd8125238979cce3f3
GathuBoswell <gathudev@gmail.com>
2016-12-19 23:05:21 +0300

    Add a test for the wordfrequency function

--- twittersent_analysis_test.py
+++ twittersent_analysis_test.py
@@ -7,7 +7,12 @@ class TwitterDataTestCase(unittest.TestCase):
     def test_twiter_data_instance(self):
         tweets = twitter_data.GetData()
         self.assertEqual(type(tweets), type(twitter_data.GetData()))
-
+    def test_twitter_data_word_frequency(self):
+        tweets = twitter_data.GetData()
+        tweets.words = ['this is and this is the way and the right way ']
+        frequency = tweets.word_frequency()
+        result = {'this':2, 'is':2, 'and':2, 'the':2, 'way':2, 'right':1}
+        self.assertEqual(result, frequency)
 
 if __name__ == '__main__':
     unittest.main()
