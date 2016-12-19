a248ed35d9dd011ff2a491de9ed00d9183305757
GathuBoswell <gathudev@gmail.com>
2016-12-19 23:19:26 +0300

    make sure the words are received as a list

--- twittersent_analysis_test.py
+++ twittersent_analysis_test.py
@@ -9,7 +9,7 @@ class TwitterDataTestCase(unittest.TestCase):
         self.assertEqual(type(tweets), type(twitter_data.GetData()))
     def test_twitter_data_word_frequency(self):
         tweets = twitter_data.GetData()
-        tweets.words = ['this is and this is the way and the right way ']
+        tweets.words = [w for w in 'this is and this is the way and the right way'.split()]
         frequency = tweets.word_frequency()
         result = {'this':2, 'is':2, 'and':2, 'the':2, 'way':2, 'right':1}
         self.assertEqual(result, frequency)
