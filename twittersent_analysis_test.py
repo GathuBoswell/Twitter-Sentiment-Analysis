06c5d98757704837e83917a20986db4683721d54
GathuBoswell <gathudev@gmail.com>
2016-12-19 21:29:31 +0300

    Remove error on the test

--- twittersent_analysis_test.py
+++ twittersent_analysis_test.py
@@ -6,7 +6,7 @@ import twitter_data
 class TwitterDataTestCase(unittest.TestCase):
     def test_twiter_data_instance(self):
         tweets = twitter_data.GetData()
-        self.assertEqual(type(tweets), twitter_data.GetData())
+        self.assertEqual(type(tweets), type(twitter_data.GetData()))
 
 
 if __name__ == '__main__':
